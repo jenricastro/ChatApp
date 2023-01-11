2180b4c9e469257a6cbcc83b059147e6b81f1e9a
Jaime Castro <jenricastro@hotmail.com>
2023-01-11 18:46:39 -0500

    server connected

--- ChatApp/MVVM/ViewModel/MainViewModel.cs
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Client.MVVM.Core;
+using Client.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +11,14 @@ namespace Client.MVVM.ViewModel
 {
     class MainViewModel
     {
-        public MainViewModel() 
+
+        public RelayCommand ConnectToServerCommand{ get; set; } 
+
+        private Server _server;
+        public MainViewModel()
         {
-        
+        _server= new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
+namespace Server
+{
+        class Program
+        {
+        static List<ServerClient> _users;
+            static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            _users= new List<ServerClient>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 3000);
+            _listener.Start();
 
+            while(true)
+            {
+                var client = new ServerClient (_listener.AcceptTcpClient());
+                _users.Add(client);
+
+                /*Send message - connected with everyone on server*/
+
+            }
+
+            
+        }
+    }
+}
